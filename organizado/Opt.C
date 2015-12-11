f6c3790ccfb71c9518e913edbeecd4586f4801e0
GCastro <gcabritac@gmail.com>
2015-12-11 15:07:56 +0000

    coisas

--- organizado/Opt.C
+++ organizado/Opt.C
@@ -70,10 +70,10 @@ TGraphErrors* Opt::Ajuste(TGraphErrors* gr1)
   TF1 *f1 = new TF1("f1",func.c_str());
 
 
-  f1->SetParameters(1,0.001,1000,800.,10);
+  f1->SetParameters(1,0.001,1000,400.,10);
   //f1->SetParLimits(4,0,100000);
   //f1->SetParLimits(3,810,830);
-  gr1->Fit("f1","MF","",400.,1024.);
+  gr1->Fit("f1","MF","",200.,500.);
 
   cout << gr1->GetErrorY(0) << " " << gr1->GetErrorY(1) << " " << gr1->GetErrorY(2) << " " << gr1->GetErrorY(3) << endl;
   cout << gr1->GetErrorX(0) << " " << gr1->GetErrorX(1) << " " << gr1->GetErrorX(2) << " " << gr1->GetErrorX(3) << endl;
