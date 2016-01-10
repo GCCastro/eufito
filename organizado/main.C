51e1a7f17a12362de725736ea32296c12f886e35
AntonioJC <antli3@hotmail.com>
2016-01-10 16:30:23 +0000

    plot

--- organizado/main.C
+++ organizado/main.C
@@ -93,7 +93,10 @@ int main(int argc, char **argv)
     gPad->Update();
 
     TPaveStats *stats1 = (TPaveStats*)gr->GetListOfFunctions()->FindObject("stats");
-    stats1->SetX1NDC(0.7); stats1->SetY1NDC(0.7);
+    stats1->SetX1NDC(.6); 
+    stats1->SetX2NDC(.85);
+    stats1->SetY1NDC(0.2);
+    stats1->SetY2NDC(0.4);
 
   }
 
